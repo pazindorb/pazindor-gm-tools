dc01d44e798eeb7e5db23e297db04685f0b00c3a
pazindorb <xxpazindorxx@gmail.com>
2026-01-24 20:59:46 +0100

    update rollup conifg

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,18 @@
 export default {
   input: "./module/pazindor-gm-tools.mjs",
-  external: (id) => id.startsWith("/modules/"),
-  makeAbsoluteExternalsRelative: false,
+
+  plugins: [
+    {
+      name: "rewrite-foundry-import",
+      resolveId(source) {
+        if (source === "/modules/pazindor-dev-essentials/module/dialog/base-dialog.mjs") {
+          return { id: "/modules/pazindor-dev-essentials/module/pazindor-dev-essentials.mjs", external: true, };
+        }
+        return null;
+      },
+    },
+  ],
+
   output: {
     file: "./release/module/pazindor-gm-tools.mjs",
     format: "es",
